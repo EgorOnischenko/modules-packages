87f066e1389cc176a422f81755de2174563d6c7c
EgorOnischenko <75452718+EgorOnischenko@users.noreply.github.com>
2022-02-27 14:58:25 +0100

    Add files via upload

---
+++ people.py
@@ -0,0 +1,10 @@
+print('start people.py')
+from datetime import datetime
+
+def get_employees():
+    time = datetime.now()
+    return print("people.py", time)
+
+
+
+print('end people.py')
---
+++ test_file.py
@@ -0,0 +1,11 @@
+from packet_with_modules import datetime
+from packet_with_modules import  calculate_salary
+from packet_with_modules import get_employees
+import time
+
+
+
+get_employees()
+time.sleep(2)
+calculate_salary()
+
